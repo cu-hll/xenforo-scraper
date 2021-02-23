4bc697d57e6a5286136b38fa01690f9bdded650d
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-02-23 04:33:58 -0500

    deps: get xo working and lint.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import client from "./mongo-client";
+import client from "./mongo-client.js";
 
 async function run() {
   try {
     await client.connect();
 
-    await client.db("admin").command({ ping: 1});
+    await client.db("admin").command({ ping: 1 });
     console.log("connected to server");
   } finally {
     console.log("in the finally.");
-    await client.close()
+    await client.close();
   }
 }
+
 run().catch(console.dir);
--- src/mongo-client.js
+++ src/mongo-client.js
@@ -2,6 +2,4 @@ import { MongoClient } from "mongodb";
 
 const uri = "mongodb://localhost:27017";
 
-export default new MongoClient(uri,
-  { useUnifiedTopology: true } 
-);
+export default new MongoClient(uri, { useUnifiedTopology: true });
